d0288c090d73803826b1990c3a869df9dcb01af3
Moniaar <omniaahmed1562001@gmail.com>
2023-08-11 16:40:53 -0700

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,10 @@ int main(void)
 int r;
 for (r = 0 ; r < 10 ; r++)
 {	putchar(r + '0');
-	if
-	(r != 9)
+	if (r != 9)
 		putchar(',');
 		putchar(' ');
-	else
+	else if
 		putchar('$');
 }
 return (0);
