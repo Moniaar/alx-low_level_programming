8d8dcc60f7da47f7c69751f447490bd46f0edffb
Moniaar <omniaahmed1562001@gmail.com>
2023-08-11 14:12:44 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry Point
+ *
+ * Description: This program wil print digits from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+for (n = 0 ; n < 10 ; n++)
+{	printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
