52bfa3d0f84e6670a30d0e5f524fa8d21de04c32
Moniaar <omniaahmed1562001@gmail.com>
2023-08-11 14:55:38 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program will print the Alphapets for you
+ *in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char r;
+for (r = 122 ; r <= 97 ; r++)
+{
+	putchar(r);
+}
+putchar('\n');
+return (0);
+}
