c3f59c396c8a241fe52dc1ccbd29191d94a9f4a0
Moniaar <omniaahmed1562001@gmail.com>
2023-08-11 00:07:34 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: C program that will use a puts function to print a line
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts(\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A program that will print a line using printf
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
