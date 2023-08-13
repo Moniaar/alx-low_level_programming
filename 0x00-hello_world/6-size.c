05e93883157eabd4098f4ffa2d5fddd342a3ce70
Moniaar <omniaahmed1562001@gmail.com>
2023-08-13 09:12:24 -0700

    a script that generates the assembly code (Intel syntax) of a C code and save it in an output file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of a char: %u bytes(s)\n", sizeof(char));
 	printf("Size of an int: %u bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %u bytes(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %lld bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %u bytes(s)\n", sizeof(float));
 	return (0);
 }
