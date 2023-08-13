95c8abc941d13f57ef05740bf08a9ef1e6d360b2
Moniaar <omniaahmed1562001@gmail.com>
2023-08-13 08:48:19 -0700

    6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <sdtio.h>
+
+/**
+ * main Entry - Point
+ *
+ * Description: We will use the function sizeOf in
+ * this program to know the size of every type we can configure
+ * 
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %u bytes(s)\n", sizeof(float));
+	return (0);
+}
