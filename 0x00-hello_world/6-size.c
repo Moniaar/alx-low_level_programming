e6c5c15c6d734d267b74f677845334241c40cd64
Moniaar <omniaahmed1562001@gmail.com>
2023-08-14 09:02:52 -0700

    this size one is a killer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry Point
  *
- * Description: We will use the function sizeOf in
- * this program to know the size of every type we can configure
+ * Description: Using sizeOf for knowing differnet
+ * sizes of variables
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("Size of a char: %u bytes(s)\n", sizeof(char));
-	printf("Size of an int: %u bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %u bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %u bytes(s)\n", sizeof(float));
+		printf("Size of a char: %c bytes(s)\n", sizeof(char));
+		printf("Size of an int: %d bytes(s)\n", sizeof(int));
+		printf("Size of a long int: %ld bytes(s)\n", sizeof(long));
+		printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+		printf("Size of a float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
