b0f4fa4af0d48c39f75db4dddea293ccf96e769c
Moniaar <omniaahmed1562001@gmail.com>
2023-08-15 13:55:12 -0700

    printinf the alphabet cuz i can't sleep

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  * print_alphabet - A function to print the Alphabet in lower
  * case followed bt a new line.
  *
- * @r: character to print
 */
 
 void print_alphabet(void)
 {
-	while (r >= 'a' || r <= 'z')
+	char r = 97;
+
+	while (r >= 97 || r <= 122)
 	{
 		_putchar(r);
 		r++;
