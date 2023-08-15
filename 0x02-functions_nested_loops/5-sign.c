464be541c3fb662d6070a5f5bca4fc7da83bd75a
Moniaar <omniaahmed1562001@gmail.com>
2023-08-15 15:21:37 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar(43 + '0');
+	{
+		_putchar(43);
 		return (1);
+	}
 	else if (n < 0)
-		_putchar(45 + '0');
+	{
+		_putchar(45);
 		return (-1);
+	}
 	else if
-		_putchar(48 + '0');
-	return (0);
+		{
+		_putchar(48);
+		return (0);
+		}
 }
 
