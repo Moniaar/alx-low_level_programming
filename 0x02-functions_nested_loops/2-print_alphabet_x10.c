4050a8dead12f55d2c7acfc98891f027e78b6a42
Moniaar <omniaahmed1562001@gmail.com>
2023-08-15 14:15:26 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - A function to print the Alphabet in lower
+ * case 10 times followed bt a new line.
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int o = 0;
+
+	while (0 < 10)
+	{
+		char r = 97;
+
+		while (r >= 97 && r <= 122)
+		{
+			_putchar(r);
+			r++;
+		}
+	 _putchar('\n');
+	0++;
+	}
+}
