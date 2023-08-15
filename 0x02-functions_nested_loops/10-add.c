9e0a0aac694649887a692594248928c0af82abd1
Moniaar <omniaahmed1562001@gmail.com>
2023-08-15 16:06:05 -0700

    adding 2 nummbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ *
+ * @a: input
+ *
+ * @b: input
+ *
+ * Return: sum of the two numbers
+*/
+
+int add(int a, int b)
+{
+	sum = a + b;
+	return (sum);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int add(int, int);
 
 
 
