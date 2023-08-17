354ee70a4d283cfd2cd97a6421e86295c148dc6b
Moniaar <omniaahmed1562001@gmail.com>
2023-08-17 01:41:25 -0700

    Adding numbers into 10 lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - A function that will print from 0
+ * to 14 ten times
+ *
+ * Description: a program that will print from 0
+ * to 14 ten times
+ *
+ * Return: Always 0 (Success)
+*/
+
+void more_numbers(void)
+{
+	int x;
+
+	int u;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (u = 0; u <= 14; u++)
+		{
+			_puctchar(u + '0');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
