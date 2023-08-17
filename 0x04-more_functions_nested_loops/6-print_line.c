f6da97ea3f28cb10e22ffd5d8b0a196fb889859b
Moniaar <omniaahmed1562001@gmail.com>
2023-08-17 14:05:59 -0700

    A program that will print a line for you

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - A function that will give you a straight
+ * line if it's biggers than 0
+ *
+ * Description: a that will give you a straight
+ * line if it's biggers than 0
+ *
+ * @n: input character
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_line(int n)
+{
+	int n = 1;
+
+	while (n > 0)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+			_putchar(95);
+	n++;
+	}
+	return (0);
+}
