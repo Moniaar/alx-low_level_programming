22eb1dfcca5ed67030d12173049e0bffc8aa962a
Moniaar <omniaahmed1562001@gmail.com>
2023-08-17 14:24:52 -0700

    A program that will print a diagonal line for you

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - A function that will print a 
+ * diagonal using \
+ *
+ * Description: a program that will print a 
+ * diagonal using \
+ *
+ * @n: input number of times the character \ should be printed
+ *
+ * @u: input
+ *
+ * @p: input
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_diagonal(int n)
+{
+	int n;
+
+	for (int u = 0; u < n; u++)
+	{
+		for ( int p = 0; p < n; p++)
+		{
+			if (p == u)
+				_putchar(92);
+			else
+				_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
