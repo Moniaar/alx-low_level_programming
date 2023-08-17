49b4f4fd5de9be7baa68f2d8fe210bb0f343af56
Moniaar <omniaahmed1562001@gmail.com>
2023-08-17 15:03:47 -0700

    My FIZZBUZZ addtion

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This is the fizzBuzz famous program
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 1 ; x <= 100 ; x++)
+	{
+		if (x % 3 == 0)
+			printf("Fizz");
+			printf(" ");
+		else if (x % 5 == 0)
+			printf("Buzz");
+			printf(" ");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+			printf(" ");
+		else
+			printf("%d", x);
+	}
+	return (0);
+}
+
