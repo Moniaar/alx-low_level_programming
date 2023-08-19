1d281f293031a6acc31fc8234d7663b867661b83
Moniaar <omniaahmed1562001@gmail.com>
2023-08-19 08:32:27 -0700

    it's getting worse lol

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,13 +15,12 @@ void print_remaining_days(int month, int day, int year)
 	 * Leap Year only when divided by 100
 	 * and 400 or 4 evenly
 	 */
-	if ((year % 100 == 0) && (year % 400 == 0) || (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
