27db52b54df5539bc44ba032d3c109131a65cc16
Moniaar <omniaahmed1562001@gmail.com>
2023-08-21 12:25:28 -0700

    it's all gonna be alright

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - a function that prints a string, followed by a new line
+ *
+ * @str: input characters of a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	do {
+		_putchar(*str + 0);
+		str++;
+	}
+	while (*str != '\0')
+		_putchar('\n');
+}
+
