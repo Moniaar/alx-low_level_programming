6636abb6cb84b809054f7a6a3305ec0d8ce338b3
Moniaar <omniaahmed1562001@gmail.com>
2023-08-21 12:07:22 -0700

    This is gonna be a great night

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - A function to return the length of a string
+ *
+ * @s: input character
+ *
+ * Return: int length of a string
+*/
+
+int _strlen(char *s)
+{
+	int cnu;
+
+	for (cnu = 0 ; *s != EOF ; *s++)
+	{
+		cnu++
+	}
+	return (cnu);
+}
