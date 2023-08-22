7411632cefd480667953ea60b4a29f7880bc3022
Moniaar <omniaahmed1562001@gmail.com>
2023-08-22 00:14:10 -0700

    nothing is hard

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string -  a function that reverses a string
+ *
+ * @s: input string
+ *
+ * Return: Always 0 (Success)
+*/
+
+void rev_string(char *s);
+{
+	int y, j;
+	char k;
+
+	for (y = 0 ; s[y] != '\0' ; y++)
+		for (j = 0 ; j < y / 2 ; j++)
+		{
+			k = s[j];
+			s[j] = s[y - 1 - j];
+			s[y - 1 - j] = k;
+		}
+}
