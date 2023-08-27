3c5d669fa1e7853121a66f9e0f7284489a2accfc
Moniaar <omniaahmed1562001@gmail.com>
2023-08-27 11:43:59 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string -  a function that capitalizes all words of a string.
+ *
+ * cptr: character pointer
+ *
+ * Return: 1 if it's uppercase.
+ * 0 if not.
+*/
+
+char *cap_string(char *)
+{
+	char *cptr;
+	char sep[] = {, , ;, ., !, ?, ",}
+
