621d01bd79861ebf672ff9f5cbb7cc1eb225a45c
Moniaar <omniaahmed1562001@gmail.com>
2023-08-30 07:31:32 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -13,10 +13,10 @@ int factorial(int n)
 {
 	if (n == 0)
 		return (1);
-	if else (n < 0)
+	else if (n < 0)
 	{	return (-1);
 	}	/*error*/
-	if else (n == 1)
+	else if (n == 1)
 		return (1);
 	else 
 		return (n * factorial(n-1));
