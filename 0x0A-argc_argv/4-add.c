8b598adc7ff1defbdececd85521f11814a8f976c
Moniaar <omniaahmed1562001@gmail.com>
2023-09-01 16:23:37 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	{
 		for (c = argv[argc] ; *c ; c++)
 			if (*c < '0' || *c > '9')
-				return (printf("Error\n"), 1);
+				return printf("Error\n");
 		s += atoi(argv[argc]);
 	}
 	printf("%d\n", s);
