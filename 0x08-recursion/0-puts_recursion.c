ec3e4b54dc6c7c11a56047cc3dbdb2cda33ac7b3
Moniaar <omniaahmed1562001@gmail.com>
2023-09-03 10:43:29 -0700

    i'm trying forever untill i die

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,10 +14,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-		_putchar('\n');
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
 }
