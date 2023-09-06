38e3ef0cfbfdc3ba0a0f384a9e8fbd7127bb3b6f
Moniaar <omniaahmed1562001@gmail.com>
2023-09-06 08:30:19 -0700

    okay here we go again

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * alloc_grid - a function that returns a pointer
@@ -14,5 +15,21 @@
 
 int **alloc_grid(int width, int height)
 {
+	int **parr;
+	int i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	parr = malloc(sizeof(int) * width);
+
+	if (parr == NULL)
+		return (NULL);
+	for (i = 0 ; i < width ; i++)
+	{
+		j = 0;
+		parr = malloc(sizeof(int) * height);
+			parr[i][j] = 0;
+	}
+	free (parr);
+	return (parr);
 }
