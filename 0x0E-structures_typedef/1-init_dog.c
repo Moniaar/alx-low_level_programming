463aabba1af1e63aeec27e097307b244bb7520b1
Moniaar <omniaahmed1562001@gmail.com>
2023-09-11 11:43:54 -0700

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,7 +17,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,11 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-		printf("Name:(nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == NULL)
-		printf("Age:(nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner:(nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	if (d)
+	{
+
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age ? d->age : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
 }
