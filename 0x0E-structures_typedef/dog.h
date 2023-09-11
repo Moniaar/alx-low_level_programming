02af9545fbee438846c2867ac247058fdf9637e8
Moniaar <omniaahmed1562001@gmail.com>
2023-09-11 10:42:21 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a strcutre about dogs
+ *
+ * @name: input
+ *
+ * @age: float input
+ *
+ * @owner: int input
+ *
+ * Description: a structure for a cute dogie
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
