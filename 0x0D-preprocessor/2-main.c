9a06b4601673e900d87e6d2ca75d45bef74572bc
Moniaar <omniaahmed1562001@gmail.com>
2023-09-11 17:28:21 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print the name of the file
+ * it was compiled from, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this is my readme file for our 0x0D-preprocessor projects
