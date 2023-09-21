68cacf0fd950aec81a499848f0e433153e249a7a
Moniaar <www.omniaahmed1562001@gmail.com>
2023-09-21 16:47:29 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,7 @@
-#include "list.h"
+#include "lists.h"
 
-/** free_list - frees the list of the all of the nodes
+/**
+ * free_list - frees the list of the all of the nodes
  *
  * @head: head of the nodes
  *
@@ -9,16 +10,16 @@
 
 void free_list(list_t *head)
 {
-	list_t *node, *nnode;
+	list_t *node, *nextnode;
 
 	if (!head)
 		return;
 	node = head;
 	while (node)
 	{
-		next_n = node->next;
+		nextnode = node->next;
 		free(node->str);
 		free(node);
-		node = next_n;
+		node = nextnode;
 	}
 }
