86ff766f958167c953ab2ecc748b867d30f30688
Moniaar <www.omniaahmed1562001@gmail.com>
2023-09-25 11:07:01 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newNode;
 		return (newNode);
 	}
-	node = *head;
+	Node = *head;
 	while (Node)
 	{
 		if (k == idx - 1)
