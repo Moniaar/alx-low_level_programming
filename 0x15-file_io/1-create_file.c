630e3a691ebd350dbd6136d21258340123abd70b
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-02 07:01:29 -0700

    Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * _stl - a function to return the length of a string
+ *
+ * @string: the string to be checked
+ *
+ * Return: int value of length
+ */
+
+int _stl(char *string)
+{
+	int k;
+
+	k = 0;
+
+	if (!string)
+	{
+		return (0);
+	}
+	while (*string++)
+		k++;
+	return (k);
+}
+
+/**
  * create_file - a function that appends text at the end of a file.
  *
  * @filename: new file to be created
@@ -12,3 +35,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int file;
+	ssize_t le, by;
+
+	le = _stl(text_content);
+	by = 0;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (file == -1)
+		return (-1);
+	if (le)
+		by = write(file, text_content, le);
+	close(file);
+	return (by == le ? 1 : -1);
+}
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,26 @@
 #include "main.h"
+/**
+ * _stl - a function to return the length of a string
+ *
+ * @string: the string to be checked
+ *
+ * Return: int value of length
+ */
+
+int _stl(char *string)
+{
+	int k;
+
+	k = 0;
+
+	if (!string)
+	{
+		return (0);
+	}
+	while (*string++)
+		k++;
+	return (k);
+}
 
 /**
  * append_text_to_file - a function that appends text at the end of a file.
@@ -12,3 +34,21 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file;
+	ssize_t le, by;
+
+	le = _stl(text_content);
+	by = 0;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+	if (le)
+		by = write(file, text_content, le);
+	close(file);
+	return (by == le ? 1 : -1);
+}
