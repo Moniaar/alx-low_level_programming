6e3a87a25ba2f82c2cf3fc400547f9242f0ff3e2
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-13 03:30:02 -0800

    Adding the function 9-strcpy to be part of the dynamic library

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - a function that copies the string pointed to by src
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ *
+ *@src: input string of character
+ *
+ *@dest: input string of character
+ *
+ * Return: character (Success)
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int y = -1;
+
+	while (src[y] != '\0')
+	{
+		y++;
+		dest[y] = src[y];
+	}
+
+	return (dest);
+}
