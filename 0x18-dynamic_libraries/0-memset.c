f0d2243853ebb86a8897bd71598df839d58d5553
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-13 02:58:48 -0800

    Adding the function 0-memset  to be part of the dynamic library

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * *_memset - a function that fills memory with a constant byte.
+ *
+ * @n: bytes of the memory area
+ *
+ * @s: input pointer
+ *
+ * @b: constant byte
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int p;
+
+	p = 0;
+
+	for (p = 0 ; n > 0 ; p++)
+	{
+		s[p] = b;
+		n--;
+	}
+	return (s);
+}
