0bf6a345c7eedcbf3beaa551d30ebf16cc65adaa
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-13 03:04:22 -0800

    Adding the function 1-strncpy  to be part of the dynamic library

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncat - function is similar to the _strcat function, except that
+ * it will use at most n bytes from src
+ *
+ * @dest: input string
+ *
+ * @src: input string
+ *
+ * @n: bytes from src
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int m, i;
+
+	m = 0;
+
+	while (dest[m])
+		m++;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[m + i] = src[i];
+
+	dest[m + i] = '\0';
+	return (dest);
+}
