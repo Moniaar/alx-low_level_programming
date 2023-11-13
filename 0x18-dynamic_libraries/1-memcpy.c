815017af3aa02d206ca82f1f8aff51172ff29ea5
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-13 03:02:36 -0800

    Adding the function 1-memcpy  to be part of the dynamic library

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * *_memcpy - a function that copies memory area
+ *
+ * @n: bytes as an input from user
+ *
+ * @src: memory Area
+ *
+ * @dest: memory area
+ *
+ * Return: a pointer to dest
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int p;
+
+	for (p = 0 ; p < n ; p++)
+	{
+		dest[p] = src[p];
+	}
+	return (dest);
+}
