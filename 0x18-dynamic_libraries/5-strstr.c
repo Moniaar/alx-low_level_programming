61cf0f3e790c634aa69814530936999ad82431a8
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-13 03:27:12 -0800

    Adding the function 5-strstr to be part of the dynamic library

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * *_strstr - a function that locates a substring.
+ *
+ * @needle: substring
+ *
+ * @haystack: string
+ *
+ * Return: a pointer to the beginning of the located substring.
+ * NULL if substring not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+}
+
