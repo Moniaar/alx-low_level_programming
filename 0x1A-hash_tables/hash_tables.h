1e7400210543957d9327518887374ad4e6909336
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2023-11-30 18:31:07 +0000

    go please

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,4 +38,5 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+void hash_table_print(const hash_table_t *ht);
 #endif
