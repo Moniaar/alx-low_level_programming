f1828e0d9bf4ee18fecd53b5b4e74354af90ef54
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2023-11-30 18:13:44 +0000

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,61 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds or updates a key/value pair in the hash table
+ * key_index - the give the index of a key
  *
- * @ht: The hash table
+ * @size: size of the hash table
  *
- * @key: The key to add or update
+ * @key: The key to get the index of
  *
- * @value: The value associated with the key
- *
- * Return: 1 if successful, 0 otherwise
+ * Return: index of key
  */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *current_node;
-
-    if (key == NULL || key[0] == '\0')
-        return (0);
-
-    index = hash_djb2((const unsigned char *)key) % ht->size;
-
-    current_node = ht->array[index];
-    while (current_node != NULL)
-    {
-        if (strcmp(current_node->key, key) == 0)
-        {
-            free(current_node->value);
-            current_node->value = strdup(value);
-            if (current_node->value == NULL)
-                return (0);
-            return (1);
-        }
-        current_node = current_node->next;
-    }
-
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
-
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
-
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return (1);
+	return (hash_djb2(key) % size);
 }
