6ff340cce8b7c544bb732a6c8c8460a7fe77bfd9
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2023-11-30 17:39:47 +0000

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,21 +2,21 @@
 
 /**
  * hash_table_create - Creates a hash table
+ *
  * @size: The size of the array
  *
  * Return: A pointer to the newly created hash table, or NULL if something went wrong
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *new_table = NULL;
     unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
     new_table = malloc(sizeof(hash_table_t));
     if (new_table == NULL)
         return (NULL);
 
-    /* Allocate memory for the array of pointers to nodes */
     new_table->array = malloc(sizeof(hash_node_t *) * size);
     if (new_table->array == NULL)
     {
@@ -26,7 +26,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
     new_table->size = size;
 
-    /* Initialize each element of the array to NULL */
     for (i = 0; i < size; i++)
         new_table->array[i] = NULL;
 
