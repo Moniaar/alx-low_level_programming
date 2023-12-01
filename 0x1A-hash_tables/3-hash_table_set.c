6ec466e85d3d2bc15f9fae549c61f35424d1d2ea
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-30 22:37:04 -0800

    Fixed the return statement here

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -54,7 +54,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 			key == NULL || strlen(key) == 0 || value == NULL)
-		return (NULL);
+		return (0);
 	indexm = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[indexm];
 	while (temp != NULL)
