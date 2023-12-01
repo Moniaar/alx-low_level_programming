474e102a7c607955a9be4304417b13095c9a6e59
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-30 22:42:13 -0800

    Add hash_table_get function to the list of functions

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 void hash_table_print(const hash_table_t *ht);
 hash_node_t *make_newhNode(const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 unsigned long int hash_djb2(const unsigned char *str);
 #endif
