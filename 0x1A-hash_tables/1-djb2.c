2e1a31fba95dad62fb2bd3d43a76dedee35a8d40
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-30 22:09:19 -0800

    1. djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,22 +1,22 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
+ * hash_djb2 - a hash function implementing the djb2 algorithm
  *
- * @str: string used to generate hash value
+ * @str: string used to generate the hash value
  *
- * Return: hash value
+ * Return: Hash value
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
