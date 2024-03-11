c128738356f6a448d58363369407fac0595e865d
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-11 14:19:54 +0200

    Testing phase

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - a function that searches for a value
+ * 10 in an array of integers using the Linear search algorithm
+ *
+ * array: the array the searching algorithm will work on
+ *
+ * size: the size of the given array
+ *
+ * value: the value we are searching for inside the array
+ *
+/
+
+int linear_search(int *array, size_t size, int value)
+{
+	int POST;
+	POST = 0;
+
+	for (POST = 0; POST < size; POST++)
+	{
+		print ("Value checked array[%d] = [%d]", array[POST], value);
+		if (array[POST] == value)
+			return (POST);
+			print("Found %d at index: %d", value, POST);
+		else
+			return (-1);
+	}
+}
