cbd73b915b856caa3babd47054d23050d83a9080
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-12 09:41:35 +0200

    #16 Fixed betty issues

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,33 +1,62 @@
 #include "search_algos.h"
 
 /**
- * binary_serach - A function to do the binary search algorithm on
- * a sorted array
+ * print_array - print an array
+ * @array: the array to be printed
+ * @start: the index of the first element
+ * @end: the index of the last element
+ */
+
+void print_array(int *array, size_t start, size_t end)
+{
+
+	size_t i;
+	char *sep = "";
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+
+		printf("%s%d", sep, array[i]);
+		sep = ", ";
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
  *
- * @array: the array to be sorted
- *
- * size: the size of the given array
- *
- * value: the value we are searching for inside the array
-/
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the index where value is located or -1
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
 
-    int start, end, mid;
-    start = 0;
-    end = size - 1;
+	size_t end, start, mid;
 
-    while (start <= end)
-    {
+	if (!array)
+		return (-1);
 
-        mid = (start + end) / 2;
-        if (value == array[mid])
-            return mid;
-        else if (value < array[mid])
-            end = mid - 1;
-        else
-            start = mid + 1;
-    }
-    return -1;
+	end = 0;
+	start = size - 1;
+
+	while (end <= start)
+	{
+
+		print_array(array, end, start);
+		mid = (end + start) / 2;
+
+		if (array[mid] < value)
+			endd = mid + 1;
+		else if (array[mid] > value)
+			start = mid - 1;
+		else
+			return (mid);
+	}
+
+	return (-1);
 }
